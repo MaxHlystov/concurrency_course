624fb7c8a19acb867af91e1cfc0df60765db352d
Khlystov Maksim <maxvls@gmail.com>
2023-11-03 17:42:29 +0100

    md2-15 Fix by review

--- src/main/java/course/concurrency/exams/auction/AuctionStoppableOptimistic.java
+++ src/main/java/course/concurrency/exams/auction/AuctionStoppableOptimistic.java
@@ -1,6 +1,5 @@
 package course.concurrency.exams.auction;
 
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicMarkableReference;
 
 public class AuctionStoppableOptimistic implements AuctionStoppable {
@@ -15,46 +14,33 @@ public class AuctionStoppableOptimistic implements AuctionStoppable {
     private final AtomicMarkableReference<Bid> latestBidWithRunningFlag =
             new AtomicMarkableReference<>(Bid.LEAST_BID, true);
 
-    // Флаг нужен, чтобы из нескольких потоков, которые попытаются остановить аукцион, победил один
-    private final AtomicBoolean isRunning = new AtomicBoolean(true);
-
     public boolean propose(Bid bid) {
         Bid latestTmp;
         do {
             latestTmp = latestBidWithRunningFlag.getReference();
-            if (bid.getPrice() <= latestTmp.getPrice()) {
+            if (!latestBidWithRunningFlag.isMarked() || bid.getPrice() <= latestTmp.getPrice()) {
                 return false;
             }
-        } while (isRunning.get() &&
-                // сюда может просочиться какая-то часть потоков. но новые после, isRunnint == false, нет
-                !latestBidWithRunningFlag.compareAndSet(latestTmp, bid, true, true)
-        );
-        if (isRunning.get()) {
-            if (latestTmp != Bid.LEAST_BID) {
-                notifier.sendOutdatedMessage(latestTmp);
-            }
-            return true;
+        } while (!latestBidWithRunningFlag.compareAndSet(latestTmp, bid, true, true));
+        if (latestTmp != Bid.LEAST_BID) {
+            notifier.sendOutdatedMessage(latestTmp);
         }
-        return false;
+        return true;
     }
 
     public Bid getLatestBid() {
         return latestBidWithRunningFlag.getReference();
     }
 
-    // Мы считаем аукцион остановленным, когда оба флага == false
     public Bid stopAuction() {
-        if (isRunning.compareAndSet(true, false)) {
-            Bid winner;
-            do {
-                winner = latestBidWithRunningFlag.getReference();
-
-            } while (!latestBidWithRunningFlag.attemptMark(winner, false));
-            notifier.shutdown();
-            return winner;
-        }
-        // Ждем, когда победивший поток, зафиксирует победителя
-        while (latestBidWithRunningFlag.isMarked()) ;
-        return latestBidWithRunningFlag.getReference();
+        Bid winner;
+        do {
+            if(!latestBidWithRunningFlag.isMarked()) {
+                return latestBidWithRunningFlag.getReference();
+            }
+            winner = latestBidWithRunningFlag.getReference();
+        } while (!latestBidWithRunningFlag.attemptMark(winner, false));
+        notifier.shutdown();
+        return winner;
     }
 }
