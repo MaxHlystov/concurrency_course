93521cdbadeac0a69f3e6771c8e94380eb889baf
Khlystov Maksim <maxvls@gmail.com>
2023-11-22 17:27:23 +0100

    md5-2 Add task 5.2.3

--- src/main/java/course/concurrency/m5_streams/ThreadPoolTask.java
+++ src/main/java/course/concurrency/m5_streams/ThreadPoolTask.java
@@ -6,11 +6,38 @@ public class ThreadPoolTask {
 
     // Task #1
     public ThreadPoolExecutor getLifoExecutor() {
-        return null;
+        return new ThreadPoolExecutor(12, 12,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingDeque<Runnable>() {
+
+                    @Override
+                    public boolean offer(Runnable e) {
+                        return super.offerFirst(e);
+                    }
+
+                    @Override
+                    public boolean offer(Runnable e, long timeout, TimeUnit unit) throws InterruptedException {
+                        return super.offerFirst(e, timeout, unit);
+                    }
+
+
+                    @Override
+                    public boolean add(Runnable e) {
+                        return super.offerFirst(e);
+                    }
+
+                    @Override
+                    public void put(Runnable e) throws InterruptedException {
+                        super.putFirst(e);
+                    }
+                });
     }
 
     // Task #2
     public ThreadPoolExecutor getRejectExecutor() {
-        return null;
+        return new ThreadPoolExecutor(8, 8,
+                0L, TimeUnit.MILLISECONDS,
+                new SynchronousQueue<>(),
+                new ThreadPoolExecutor.DiscardPolicy());
     }
 }
